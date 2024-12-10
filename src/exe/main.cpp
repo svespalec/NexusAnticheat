e13dea528701d28d3f0bd8701ffd9688d20d6994
Samuel <vespalecsamuel@gmail.com>
2024-12-10 15:53:12 -0800

    CMake Base

---
+++ src/exe/main.cpp
@@ -0,0 +1,10 @@
+// NexusAnticheat.cpp : Defines the entry point for the application.
+
+#include <Windows.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "NexusAnticheat started" << std::endl;
+    return 0;
+}
